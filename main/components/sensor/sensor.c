8c6446c6e0cda409cc65dc06716536bed14c3395
Fabio Molon <fabio.molon@athonet.com>
2024-12-12 18:18:45 +0100

    Develop function for send command and create sensor packet

--- main/components/sensor/sensor.c
+++ main/components/sensor/sensor.c
@@ -1,32 +1,86 @@
-#include <stdint.h>
 #include <string.h>
+
+#include "esp_log.h"
+
 #include "sensor.h"
 
-/* Set alarm sensor structure */
-node_id set_alarm_sensor(uint8_t id, bool state) {
+#define TAG_SENSOR "SENSOR"
 
-    node_id n;
+/* Calculate crc16 */
+static uint16_t calc_crc16(uint8_t *data, uint32_t length) {
 
-    memset(&n, 0, sizeof(n));
+    uint16_t crc = 0xFFFF;
 
-    n.id = id;
-    n.type = ALARM_SENSOR;
-    n.select.as.state = state;
+    for (uint32_t i = 0; i < length; i++) {
 
-    return n;
+        crc ^= (uint16_t)data[i];
+        for (uint8_t bit = 0; bit < 8; bit++)  {
+            if (crc & 0x0001) {
+                crc = (crc >> 1) ^ 0x8005;
+            } else {
+                crc >>= 1;
+            }
+        }
+    }
+
+    return crc;
 }
 
-/* Set metric sensor structure */
-node_id set_metric_sensor(uint8_t id, float temp, uint8_t hum) {
+/* Build command to send sensor */
+node_id_cmd build_command(uint8_t id, enum sensor_type st, enum cmd_type ct, int64_t time) {
 
-    node_id n;
+    node_id_cmd pkt;
 
-    memset(&n, 0, sizeof(n));
+    pkt.id = id;
 
-    n.id = id;
-    n.type = TEMPERATURE_HUMIDITY_SENSOR;
-    n.select.ms.temp = temp;
-    n.select.ms.hum = hum;
+    pkt.sensor = st;
+    pkt.cmd = ct;
+    pkt.time = time;
+    pkt.crc = calc_crc16((uint8_t *)&pkt, sizeof(pkt) - sizeof(pkt.crc));
 
-    return n;
+    return pkt;
+}
+
+/* Alarm sensor packet response */
+node_id_alarm set_alarm_sensor(uint8_t id, bool state, int64_t time) {
+
+    node_id_alarm pkt;
+
+    memset(&pkt, 0, sizeof(pkt));
+    pkt.id = id;
+
+	pkt.sensor = ALARM_SENSOR;
+
+    pkt.alarm_sensor.battery_low_detect = false;
+    pkt.alarm_sensor.state = state;
+
+	pkt.time = time;
+	pkt.crc = calc_crc16((uint8_t *)&pkt, sizeof(pkt) - sizeof(pkt.crc));
+
+    ESP_LOGI(TAG_SENSOR, "ID: %u", pkt.id);
+    ESP_LOGI(TAG_SENSOR, "Sensor: %u", pkt.sensor);
+    ESP_LOGI(TAG_SENSOR, "Alarm sensor state: %u", pkt.alarm_sensor.state);
+    ESP_LOGI(TAG_SENSOR, "Time: %lld", pkt.time);
+    ESP_LOGI(TAG_SENSOR, "Alarm sensor state: %u", pkt.crc);
+
+    return pkt;
+}
+
+/* Metric sensor packet response  */
+node_id_weather_metric set_metric_sensor(uint8_t id, float temp, uint8_t hum, int64_t time) {
+
+    node_id_weather_metric pkt;
+
+    memset(&pkt, 0, sizeof(pkt));
+    pkt.id = id;
+
+    pkt.sensor = TEMPERATURE_HUMIDITY_SENSOR;
+
+    pkt.metric_sensor.temp = temp;
+    pkt.metric_sensor.hum = hum;
+
+	pkt.time = time;
+	pkt.crc = calc_crc16((uint8_t *)&pkt, sizeof(pkt) - sizeof(pkt.crc));
+
+    return pkt;
 }
