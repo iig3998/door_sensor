4f4886614ce66093bfd8f7c1ba2cb383f9454ca4
Fabio Molon <fabiomolon85@gmail.com>
2024-12-12 18:18:45 +0100

    Develop function for send command and create sensor packet

--- main/components/sensor/sensor.h
+++ main/components/sensor/sensor.h
@@ -9,28 +9,49 @@ enum sensor_type {
     ALARM_SENSOR
 };
 
-typedef struct {
-	float temp;
-	uint8_t hum;
-} metric_sensor;
+enum cmd_type {
+	GET = 0,
+	SET,
+	DEL
+};
 
-typedef struct {
-	bool state;
-} alarm_sensor;
-
+/* Struct command sensor */
 typedef struct {
 	uint8_t id;
-	enum sensor_type type;
-    union {
-		metric_sensor ms;
-		alarm_sensor as;
-	} select;
-} __attribute__((__packed__)) node_id;
+	enum cmd_type cmd;
+	enum sensor_type sensor;
+	int64_t time;
+	uint16_t crc;
+} __attribute__((__packed__)) node_id_cmd;
 
-/**/
-node_id set_alarm_sensor(uint8_t id, bool state);
+/* Struct alarm sensor */
+typedef struct {
+	uint8_t id;
+	enum sensor_type sensor;
+	struct {
+		bool state;
+		bool battery_low_detect;
+	} alarm_sensor;
+	int64_t time;
+	uint16_t crc;
+} __attribute__((__packed__)) node_id_alarm;
 
-/**/
-node_id set_metric_sensor(uint8_t id, float temp, uint8_t hum);
+/* Struct metric sensor */
+typedef struct {
+	uint8_t id;
+	enum sensor_type sensor;
+	struct {
+		float temp;
+		uint8_t hum;
+	} metric_sensor;
+	int64_t time;
+	uint16_t crc;
+} __attribute__((__packed__)) node_id_weather_metric;
+
+node_id_cmd build_command(uint8_t id, enum sensor_type st, enum cmd_type ct, int64_t time);
+
+node_id_alarm set_alarm_sensor(uint8_t id, bool state, int64_t time);
+
+node_id_weather_metric set_metric_sensor(uint8_t id, float temp, uint8_t hum, int64_t time);
 
 #endif
