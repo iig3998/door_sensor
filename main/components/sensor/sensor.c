4ae13f63f5706d4ef5c86e95f8898f13bcc0cbde
Fabio Molon <fabiomolon85@gmail.com>
2024-12-19 17:20:43 +0100

    Update fileds node_id:cmd and node_id_alarm

--- main/components/sensor/sensor.c
+++ main/components/sensor/sensor.c
@@ -27,13 +27,13 @@ static uint16_t calc_crc16(uint8_t *data, uint32_t length) {
 }
 
 /* Build command to send sensor */
-node_id_cmd build_command(uint8_t id, enum sensor_type st, enum cmd_type ct, int64_t time) {
+node_id_cmd build_command(uint8_t id, enum node_type st, enum cmd_type ct, int64_t time) {
 
     node_id_cmd pkt;
 
     pkt.id = id;
 
-    pkt.sensor = st;
+    pkt.node = st;
     pkt.cmd = ct;
     pkt.time = time;
     pkt.crc = calc_crc16((uint8_t *)&pkt, sizeof(pkt) - sizeof(pkt.crc));
@@ -49,38 +49,19 @@ node_id_alarm set_alarm_sensor(uint8_t id, bool state, int64_t time) {
     memset(&pkt, 0, sizeof(pkt));
     pkt.id = id;
 
-	pkt.sensor = ALARM_SENSOR;
+	pkt.node = ALARM_SENSOR;
 
-    pkt.alarm_sensor.battery_low_detect = false;
-    pkt.alarm_sensor.state = state;
+    pkt.state = state;
+    pkt.battery_low_detect = false;
 
 	pkt.time = time;
 	pkt.crc = calc_crc16((uint8_t *)&pkt, sizeof(pkt) - sizeof(pkt.crc));
 
     ESP_LOGI(TAG_SENSOR, "ID: %u", pkt.id);
-    ESP_LOGI(TAG_SENSOR, "Sensor: %u", pkt.sensor);
-    ESP_LOGI(TAG_SENSOR, "Alarm sensor state: %u", pkt.alarm_sensor.state);
+    ESP_LOGI(TAG_SENSOR, "Node type: %u", pkt.node);
+    ESP_LOGI(TAG_SENSOR, "Alarm sensor state: %u", pkt.state);
     ESP_LOGI(TAG_SENSOR, "Time: %lld", pkt.time);
     ESP_LOGI(TAG_SENSOR, "Alarm sensor state: %u", pkt.crc);
-
-    return pkt;
-}
-
-/* Metric sensor packet response  */
-node_id_weather_metric set_metric_sensor(uint8_t id, float temp, uint8_t hum, int64_t time) {
-
-    node_id_weather_metric pkt;
-
-    memset(&pkt, 0, sizeof(pkt));
-    pkt.id = id;
-
-    pkt.sensor = TEMPERATURE_HUMIDITY_SENSOR;
-
-    pkt.metric_sensor.temp = temp;
-    pkt.metric_sensor.hum = hum;
-
-	pkt.time = time;
-	pkt.crc = calc_crc16((uint8_t *)&pkt, sizeof(pkt) - sizeof(pkt.crc));
 
     return pkt;
 }
