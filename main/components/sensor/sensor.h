30036af1aa3662d0aadabe793222678208419666
Fabio Molon <fabio.molon@athonet.com>
2024-12-19 17:20:43 +0100

    Update fileds node_id:cmd and node_id_alarm

--- main/components/sensor/sensor.h
+++ main/components/sensor/sensor.h
@@ -4,9 +4,12 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-enum sensor_type {
-    TEMPERATURE_HUMIDITY_SENSOR = 0,
-    ALARM_SENSOR
+#define SIZE_CODE 6
+
+enum node_type {
+    ALARM_SENSOR = 0,
+    SIREN_SENSOR,
+	KEYBOARD
 };
 
 enum cmd_type {
@@ -19,7 +22,7 @@ enum cmd_type {
 typedef struct {
 	uint8_t id;
 	enum cmd_type cmd;
-	enum sensor_type sensor;
+	enum node_type node;
 	int64_t time;
 	uint16_t crc;
 } __attribute__((__packed__)) node_id_cmd;
@@ -27,31 +30,16 @@ typedef struct {
 /* Struct alarm sensor */
 typedef struct {
 	uint8_t id;
-	enum sensor_type sensor;
-	struct {
-		bool state;
-		bool battery_low_detect;
-	} alarm_sensor;
+	enum node_type node;
+	bool state;
+	bool battery_low_detect;
+	char code[SIZE_CODE];
 	int64_t time;
 	uint16_t crc;
 } __attribute__((__packed__)) node_id_alarm;
 
-/* Struct metric sensor */
-typedef struct {
-	uint8_t id;
-	enum sensor_type sensor;
-	struct {
-		float temp;
-		uint8_t hum;
-	} metric_sensor;
-	int64_t time;
-	uint16_t crc;
-} __attribute__((__packed__)) node_id_weather_metric;
-
-node_id_cmd build_command(uint8_t id, enum sensor_type st, enum cmd_type ct, int64_t time);
+node_id_cmd build_command(uint8_t id, enum node_type st, enum cmd_type ct, int64_t time);
 
 node_id_alarm set_alarm_sensor(uint8_t id, bool state, int64_t time);
-
-node_id_weather_metric set_metric_sensor(uint8_t id, float temp, uint8_t hum, int64_t time);
 
 #endif
