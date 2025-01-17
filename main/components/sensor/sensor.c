8a53d9fc169c2e0cb70183d6b54b36b3a831b392
Fabio Molon <fabiomolon85@gmail.com>
2025-01-17 15:50:18 +0100

    Adds function to print sensor values

--- main/components/sensor/sensor.c
+++ main/components/sensor/sensor.c
@@ -1,10 +1,25 @@
 #include <string.h>
 
 #include "esp_log.h"
+#include "esp_mac.h"
 
 #include "sensor.h"
 
 #define TAG_SENSOR "SENSOR"
+
+/* Print node caratteristics */
+static void print_node(node_id_alarm pkt) {
+
+    ESP_LOGI(TAG_SENSOR, "Cmd: %u", pkt.header.cmd);
+    ESP_LOGI(TAG_SENSOR, "ID: %u", pkt.header.id);
+    ESP_LOGI(TAG_SENSOR, "Node type: %u", pkt.header.node);
+    ESP_LOGI(TAG_SENSOR, "Sensor state: %u", pkt.state);
+    ESP_LOGI(TAG_SENSOR, "Battery low detect: %u", pkt.battery_low_detect);
+    ESP_LOGI(TAG_SENSOR, "Time: %lld", pkt.time);
+    ESP_LOGI(TAG_SENSOR, "CRC16: %u", pkt.crc);
+
+    return;
+}
 
 /* Calculate crc16 */
 static uint16_t calc_crc16(uint8_t *data, uint32_t length) {
@@ -26,45 +41,48 @@ static uint16_t calc_crc16(uint8_t *data, uint32_t length) {
     return crc;
 }
 
-/* Build command to send sensor */
-node_id_cmd build_command(uint8_t id, enum node_type st, enum cmd_type ct, int64_t time) {
-
-    node_id_cmd pkt;
-
-    memset(&pkt, 0, sizeof(pkt));
-    pkt.id = id;
-
-    pkt.node = st;
-    pkt.cmd = ct;
-    pkt.time = time;
-    pkt.crc = calc_crc16((uint8_t *)&pkt, sizeof(pkt) - sizeof(pkt.crc));
-
-    return pkt;
-}
-
-/* Alarm sensor packet response */
-node_id_alarm set_alarm_sensor(uint8_t id, bool state, bool battery_low_detect, int64_t time) {
+/* Alarm sensor update packet */
+node_id_alarm alarm_sensor_update_pkt(uint8_t id, bool state, bool battery_low_detect, int64_t time) {
 
     node_id_alarm pkt;
 
     memset(&pkt, 0, sizeof(pkt));
-    pkt.id = id;
 
-	pkt.node = ALARM_SENSOR;
+    /* Build header packet */
+    pkt.header.cmd = UPDATE;
+    pkt.header.id = id;
+    esp_read_mac(pkt.header.mac, ESP_MAC_WIFI_STA);
+	pkt.header.node = ALARM_SENSOR;
 
     pkt.state = state;
     pkt.battery_low_detect = battery_low_detect;
-    memset(pkt.code, '\0', sizeof(pkt.code));
 	pkt.time = time;
 	pkt.crc = calc_crc16((uint8_t *)&pkt, sizeof(pkt) - sizeof(pkt.crc));
 
-    ESP_LOGI(TAG_SENSOR, "ID: %u", pkt.id);
-    ESP_LOGI(TAG_SENSOR, "Node type: %u", pkt.node);
-    ESP_LOGI(TAG_SENSOR, "Sensor state: %u", pkt.state);
-    ESP_LOGI(TAG_SENSOR, "Battery low detect: %u", pkt.battery_low_detect);
-    ESP_LOGI(TAG_SENSOR, "Code: %s", pkt.code);
-    ESP_LOGI(TAG_SENSOR, "Time: %lld", pkt.time);
-    ESP_LOGI(TAG_SENSOR, "CRC16: %u", pkt.crc);
+    print_node(pkt);
+
+    return pkt;
+}
+
+/* Alarm sensor add packet */
+node_id_alarm alarm_sensor_add_pkt(uint8_t id, int64_t time) {
+
+    node_id_alarm pkt;
+
+    memset(&pkt, 0, sizeof(pkt));
+
+    /* Build header packet */
+    pkt.header.cmd = ADD;
+    pkt.header.id = id;
+    esp_read_mac(pkt.header.mac, ESP_MAC_WIFI_STA);
+	pkt.header.node = ALARM_SENSOR;
+
+    pkt.state = false;
+    pkt.battery_low_detect = false;
+	pkt.time = time;
+	pkt.crc = calc_crc16((uint8_t *)&pkt, sizeof(pkt) - sizeof(pkt.crc));
+
+    print_node(pkt);
 
     return pkt;
 }
