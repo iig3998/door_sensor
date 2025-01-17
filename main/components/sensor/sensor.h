ed77bd91803fb4841a799f8458db2e87e83c6d9c
Fabio Molon <fabiomolon85@gmail.com>
2025-01-17 15:51:16 +0100

    Updates command list and adds struct response message

--- main/components/sensor/sensor.h
+++ main/components/sensor/sensor.h
@@ -4,42 +4,56 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define SIZE_CODE 6
-
 enum node_type {
     ALARM_SENSOR = 0,
-    SIREN_SENSOR,
-	KEYBOARD
+    SIREN,
+	KEYBOARD,
+	GATEWAY
 };
 
 enum cmd_type {
-	GET = 0,
-	SET,
-	DEL
+	ADD = 0,
+	DEL,
+	UPDATE,
+	ACTIVE,
+	DEACTIVE
 };
 
-/* Struct command sensor */
+/* Strcu header message */
 typedef struct {
+	enum node_type node;
 	uint8_t id;
+	uint8_t mac[6];
 	enum cmd_type cmd;
-	enum node_type node;
-	int64_t time;
-	uint16_t crc;
-} __attribute__((__packed__)) node_id_cmd;
+} __attribute__((__packed__)) node_id_header;
 
-/* Struct alarm sensor */
+/* Struct alarm sensor message */
 typedef struct {
-	uint8_t id;
-	enum node_type node;
+	node_id_header header;
 	bool state;
 	bool battery_low_detect;
-	char code[SIZE_CODE];
 	int64_t time;
 	uint16_t crc;
 } __attribute__((__packed__)) node_id_alarm;
 
-node_id_cmd build_command(uint8_t id, enum node_type st, enum cmd_type ct, int64_t time);
+/* Struct gateway message */
+typedef struct {
+	node_id_header header;
+	bool ack;
+	int64_t time;
+	uint16_t crc;
+} __attribute__((__packed__)) node_id_gateway;
 
-node_id_alarm set_alarm_sensor(uint8_t id, bool state, bool battery_low_detect, int64_t time);
+/* Struct response message */
+typedef struct {
+	node_id_header header;
+	bool ack;
+	int64_t time;
+	uint16_t crc;
+} __attribute__((__packed__)) node_id_response;
+
+node_id_alarm alarm_sensor_update_pkt(uint8_t id, bool state, bool battery_low_detect, int64_t time);
+
+node_id_alarm alarm_sensor_add_pkt(uint8_t id, int64_t time);
 
 #endif
